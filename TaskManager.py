46588b08deeddeb771a2fdf7fde5c61f3a6c9ac5
Ethan <carteronethan@gmail.com>
2023-06-20 15:27:38 -0400

    Update TaskManager.py
    I've added some function

--- TaskManager.py
+++ TaskManager.py
@@ -1,10 +1,13 @@
+tasksArray = []
+
 while True:
     
-    tasksArray = []
     userInput = ""
+    
     def listTask():
         for element in tasksArray:
-            print(element)
+            print("- ", element)
+            print(" ")
             
     def addTask():
         newTask = input("Which task you want to add : ")
@@ -14,18 +17,31 @@ while True:
     def menu():
         print("Enter 1 to List task")
         print("Enter 2 to Add a task")
+        print("Enter 3 to Check how many task do you have")
+        print("Enter 4 to Delete a task")
         print("Enter q to quit")
         userInput = input("")
         return userInput
+    
+    def numberOfTask():
+        print("You have", len(tasksArray), " tasks")
 
+    def deleteTask():
+        deletedTask = input("Which task you want to delete : ")
+        tasksArray.remove(deletedTask)
+        print(deletedTask, " has been deleted")
+    
     userInput = menu()
        
     if userInput == "1":
         listTask()
     elif userInput == "2":
         addTask()
+    elif userInput == "3":
+        numberOfTask()
+    elif userInput == "4":
+        deleteTask()
     elif userInput == "q":
         break
     else:
         print("Invalid choice")
-
